2dcaa79afc67c4e4bcd3fc76f0d8471c1aa0b291
TolikT <anatoliytihomirov@yahoo.com>
2016-03-15 16:09:10 +0300

    add right checking to modification test

--- addressbook-web-tests/src/test/java/ru/tikhoa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/tikhoa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tikhoa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -26,7 +27,8 @@ public class GroupModificationTests extends TestBase{
         // modify a group and go back to group page
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
 
@@ -35,5 +37,10 @@ public class GroupModificationTests extends TestBase{
 
         // compare before and after size
         Assert.assertEquals(before.size(), after.size());
+
+        // check using sets
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
