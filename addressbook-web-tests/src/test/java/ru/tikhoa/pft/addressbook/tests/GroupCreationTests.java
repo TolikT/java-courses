4e98f56ffa45e1381d8ed950012ed490b53e9180
TolikT <anatoliytihomirov@yahoo.com>
2016-03-30 09:43:57 +0100

    added parameters to GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/tikhoa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tikhoa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,16 +1,30 @@
 package ru.tikhoa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.tikhoa.pft.addressbook.model.GroupData;
 import ru.tikhoa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
 
         // go to group page
         app.goTo().groupPage();
@@ -19,7 +33,6 @@ public class GroupCreationTests extends TestBase {
         Groups before = app.group().all();
 
         // create a group
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
 
         // compare before and after size
