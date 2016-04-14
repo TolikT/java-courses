5198a704d4cd96edcf79d977cd9372ad0783fb60
TolikT <anatoliytihomirov@yahoo.com>
2016-04-14 14:29:59 +0300

    add ftpHelper for captcha disabling

--- mantis-tests/src/test/java/ru/tikhoa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/tikhoa/pft/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftpHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -49,6 +50,13 @@ public class ApplicationManager {
             registrationHelper = new RegistrationHelper(this);
         }
         return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftpHelper == null) {
+            ftpHelper = new FtpHelper(this);
+        }
+        return ftpHelper;
     }
 
     public WebDriver getDriver() {
