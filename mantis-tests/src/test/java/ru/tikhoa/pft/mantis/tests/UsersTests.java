ead93532e43641b5c27a305ad2402537d196292a
TolikT <anatoliytihomirov@yahoo.com>
2016-04-18 18:17:53 +0300

    renamed registration helper to users helper

--- mantis-tests/src/test/java/ru/tikhoa/pft/mantis/tests/UsersTests.java
+++ mantis-tests/src/test/java/ru/tikhoa/pft/mantis/tests/UsersTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
-public class RegistrationTests extends TestBase{
+public class UsersTests extends TestBase{
 
     @BeforeClass
     public void startMailServer() {
